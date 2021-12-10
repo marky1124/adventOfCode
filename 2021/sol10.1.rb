3993012f0b7f9382bf6c3b8ebe865f5a9a496c1b
marky1124 <marky1124@gmail.com>
2021-12-10 07:20:47 +0000

    feat(ruby): 2021 day 10

---
+++ 2021/sol10.1.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# Solve a puzzle from https://adventofcode.com
+class Puzzle
+  def initialize
+    @openers = '({[<'
+    @closer = { '(' => ')', '{' => '}', '[' => ']', '<' => '>' }
+    @score = { ')' => 3, ']' => 57, '}' => 1197, '>' => 25_137 }
+  end
+
+  def solve_it(filename)
+    answer = 0
+    fd = File.open(filename)
+    until fd.eof
+      line = fd.readline.chomp
+      input = line.chars
+      expect = []
+      loop do
+        c = input.shift
+        if @openers.include?(c)
+          expect.append(@closer[c])
+        else
+          c_should_be = expect.pop
+          if c != c_should_be
+            puts "line #{line} - Expected #{c_should_be}, but found #{c} instead"
+            answer += @score[c]
+          end
+        end
+        break if input.empty?
+      end
+    end
+    puts "Answer is #{answer}"
+    fd.close
+  end
+end
+
+filename = ARGV[0]
+if filename.nil? || filename.length.zero?
+  puts "Usage: #{$PROGRAM_NAME} <file>"
+  puts "  e.g: #{$PROGRAM_NAME} in24"
+  exit
+end
+
+p = Puzzle.new
+p.solve_it(filename)
---
+++ 2021/sol10.2.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'byebug'
+# Solve a puzzle from https://adventofcode.com
+class Puzzle
+  def initialize
+    @openers = '({[<'
+    @closer = { '(' => ')', '{' => '}', '[' => ']', '<' => '>' }
+    @score = { ')' => 1, ']' => 2, '}' => 3, '>' => 4 }
+    @scores = []
+  end
+
+  def solve_it(filename)
+    fd = File.open(filename)
+    until fd.eof
+      line = fd.readline.chomp
+      input = line.chars
+      expect = []
+      loop do
+        c = input.shift
+        if @openers.include?(c)
+          expect.append(@closer[c])
+        else
+          c_should_be = expect.pop
+          if c != c_should_be
+            puts "line #{line} - Expected #{c_should_be}, but found #{c} instead"
+            break
+          end
+        end
+        next unless input.empty?
+
+        # Found a valid but incomplete line, score it and move on
+        score = 0
+        expect.reverse.each do |e|
+          score *= 5
+          score += @score[e]
+        end
+        puts "line #{line} - Incomplete remaining should be #{expect.join.reverse} - scoring #{score}"
+        @scores.append(score)
+        break
+      end
+    end
+    @scores.sort!
+    puts "Scores = #{@scores}"
+    puts "Answer is #{@scores[@scores.length / 2]}"
+    fd.close
+  end
+end
+
+filename = ARGV[0]
+if filename.nil? || filename.length.zero?
+  puts "Usage: #{$PROGRAM_NAME} <file>"
+  puts "  e.g: #{$PROGRAM_NAME} in24"
+  exit
+end
+
+p = Puzzle.new
+p.solve_it(filename)
