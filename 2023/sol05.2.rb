a1355769c2e6df0cc848d01b62cc86a412bb6c31
marky1124 <marky1124@gmail.com>
2023-12-08 23:16:29 +0000

    refactor(ruby): 2023 day 5
    Refactored the algorithm to track seed ranges instead of individual
    seeds. Full run is now 4 hundredths of a second. 480,000 times
    faster than the original algorithm. :)

--- 2023/sol05.2.rb
+++ 2023/sol05.2.rb
@@ -14,11 +14,33 @@ class Map
     @mappings << [min, max, offset]
   end
 
-  def map(number)
-    @mappings.each do |min, max, offset|
-      return number + offset if number >= min && number <= max
+  def map(seed_ranges)
+    mapped_seeds = []
+    seeds_to_check = []
+    @mappings.each do |range_start, range_end, offset|
+      seeds_to_check = []
+      seed_ranges.each do |start_seed, end_seed|
+        if end_seed < range_start
+          seeds_to_check << [start_seed, end_seed]
+        elsif start_seed > range_end
+          seeds_to_check << [start_seed, end_seed]
+        elsif start_seed >= range_start && end_seed <= range_end
+          mapped_seeds << [start_seed + offset, end_seed + offset]
+        elsif start_seed < range_start && end_seed > range_end
+          seeds_to_check << [start_seed, range_start - 1]
+          mapped_seeds << [range_start + offset, range_end + offset]
+          seeds_to_check << [range_end + 1, end_seed]
+        elsif start_seed < range_start && end_seed <= range_end
+          seeds_to_check << [start_seed, range_start - 1]
+          mapped_seeds << [range_start + offset, end_seed + offset]
+        elsif start_seed >= range_start && end_seed > range_end
+          mapped_seeds << [start_seed + offset, range_end + offset]
+          seeds_to_check << [range_end + 1, end_seed]
+        end
+      end
+      seed_ranges = seeds_to_check
     end
-    number
+    mapped_seeds + seeds_to_check
   end
 end
 
@@ -29,7 +51,8 @@ end
 # @maps['seed-to-soil'].map(98) => 50
 #
 # Map.new('seed-to-soil').add_range('50 98 2')
-# Map.new('seed-to-soil').map(98) => 50
+# @seed_ranges = [[79,92],[55,67]]
+# Map.new('seed-to-soil').map(@seed_ranges) => [[81,94],[57,69]]
 
 # Solve a puzzle from https://adventofcode.com
 class Solve
@@ -37,22 +60,16 @@ class Solve
     @maps = Hash.new { |hash, key| hash[key] = [] }
     @map_names = []
 
-    process_file(ARGV[0] || 'in05')
+    process_file(ARGV[0] || 'tin05')
     puts "Answer to part 1 = #{nearest_location}"
   end
 
   def nearest_location
-    nearest_location = nil
-    @seeds.each_slice(2) do |start, length|
-      puts "#{Time.now} #{start} #{length}"
-      (start..start + length - 1).each do |number|
-        @map_names.each do |map_name|
-          number = @maps[map_name].map(number)
-        end
-        nearest_location = number if nearest_location.nil? || number < nearest_location
-      end
+    @map_names.each do |map_name|
+      @seed_ranges = @maps[map_name].map(@seed_ranges)
     end
-    nearest_location
+    @seed_ranges.sort! { |a, b| a.first <=> b.first }
+    @seed_ranges.first.first
   end
 
   def process_file(file)
@@ -60,7 +77,11 @@ class Solve
     File.readlines(file, chomp: true).each do |line|
       case line
       when /^seeds: /
-        @seeds = line.gsub(/^seeds: /, '').split.map(&:to_i)
+        seeds = line.gsub(/^seeds: /, '').split.map(&:to_i)
+        seeds.each_slice(2) do |start, length|
+          @seed_ranges ||= []
+          @seed_ranges << [start, start + length - 1]
+        end
       when /^.*map:/
         name = line.split.first
         @map_names << name
